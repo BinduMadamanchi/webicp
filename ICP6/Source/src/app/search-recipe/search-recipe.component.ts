c977941ce67d82950924e33bf55fb00f64d6fd35
BinduMadamanchi <42659031+BinduMadamanchi@users.noreply.github.com>
2019-09-28 21:11:31 -0500

    icp6

--- ICP6/Source/src/app/search-recipe/search-recipe.component.ts
+++ ICP6/Source/src/app/search-recipe/search-recipe.component.ts
@@ -13,11 +13,22 @@ export class SearchRecipeComponent implements OnInit {
   placeValue: any;
   venueList = [];
   recipeList = [];
+  formattedAddress = [];
 
   currentLat: any;
   currentLong: any;
   geolocationPosition: any;
 
+  recepieApi = 'https://api.edamam.com/search?q=';
+  recepieAppid = '&app_id=454fe335';
+  recepieKey = '&app_key=0773cc089b069e8196a3ad56c6374a42';
+
+  placesApi = 'https://api.foursquare.com/v2/venues/search?';
+  clientdId = 'client_id=AZ5YEHLFZWTZJQAO0YPRGLGIIVFQOS3MRSPFHDOZRQ1XU1PD';
+  clientSecret = '&client_secret=1HKYC2PGOKIAYIQAXDCGZOSAADRLRVND0UMF5VLRA1GVDJ4A';
+  version = '&v=20180323';
+  near1 = '&near=';
+  query = '&query=';
   constructor(private _http: HttpClient) {
   }
 
@@ -37,15 +48,26 @@ export class SearchRecipeComponent implements OnInit {
     this.placeValue = this.places.nativeElement.value;
 
     if (this.recipeValue !== null) {
-      /**
-       * Write code to get recipe
-       */
+      this._http.get( this.recepieApi + this.recipeValue + this.recepieAppid + this.recepieKey).subscribe((result: any) => {
+        console.log(result);
+        this.recipeList = Object.keys(result.hits).map(function (k) {
+          const i = result.hits[k].recipe;
+          return {name: i.label, icon: i.image, url: i.url};
+        });
+
+      });
+
     }
 
     if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
-      /**
-       * Write code to get place
-       */
+      this._http.get( this.placesApi  + this.clientdId + this.clientSecret + this.version
+        + this.near1 + this.placeValue).subscribe((result: any) => {
+        console.log(result);
+        this.venueList = Object.keys(result.response.venues).map(function (k) {
+          const i = result.response.venues[k];
+          return {name: i.name, currentLat: '39.0349657', currentLong: '-94.5787524', formattedAddress: i.location.formattedAddress};
+        });
+      });
     }
   }
 }
